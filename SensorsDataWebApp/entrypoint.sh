3050c403412866ed4b8f6e02c62ebadd85d1e9e4
louaitayyar <louaitayyar@outlook.com>
2022-12-09 12:25:38 +0200

    fixed small details

--- SensorsDataWebApp/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python3 manage.py collectstatic --noinput
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-
-gunicorn SensorsDataWebApp.wsgi:application --bind 0.0.0.0:$PORT
-
